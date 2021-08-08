6fb882ace606b4f916d2e0e99ba01d9ef50730b5
aninggo <aninggo@gmail.com>
2021-08-08 21:13:04 +0800

    update

--- vtile/mvt_test.go
+++ vtile/mvt_test.go
@@ -1,1 +1,18 @@
 package vtile
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestMVT(t *testing.T) {
+	f, _ := os.Open("../data/3194.mvt")
+	bytes, _ := ioutil.ReadAll(f)
+
+	tile := NewTile(bytes)
+
+	if tile.Empty() {
+		t.FailNow()
+	}
+}
--- wagyu/wagyu_test.go
+++ wagyu/wagyu_test.go
@@ -1,1 +1,22 @@
 package wagyu
+
+import (
+	"testing"
+
+	"github.com/flywave/go-mbgeom/geojson"
+)
+
+func TestClipRing(t *testing.T) {
+	ring := geojson.NewLinearRing([][2]float64{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{0, 1},
+		{0, 0},
+	})
+
+	ctx := NewContext()
+
+	if ctx != nil && ring != nil {
+	}
+}

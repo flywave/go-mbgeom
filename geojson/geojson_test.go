41d0c98518ca98bca3b75f1b44d7a6039aacfdfb
aninggo <aninggo@gmail.com>
2021-08-08 20:37:21 +0800

    update

--- geojson/geojson_test.go
+++ geojson/geojson_test.go
@@ -1,1 +1,18 @@
 package geojson
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestGeoJSON(t *testing.T) {
+	f, _ := os.Open("../data/countries.geojson")
+	bytes, _ := ioutil.ReadAll(f)
+
+	json := Parse(string(bytes))
+
+	if json == nil && !json.IsFeatureCollection() {
+		t.FailNow()
+	}
+}
--- geojsonvt/geojsonvt_test.go
+++ geojsonvt/geojsonvt_test.go
@@ -1,1 +1,32 @@
 package geojsonvt
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/flywave/go-mbgeom/geojson"
+)
+
+func TestGeoJSONVT(t *testing.T) {
+	f, _ := os.Open("../data/countries.geojson")
+	bytes, _ := ioutil.ReadAll(f)
+
+	json := geojson.Parse(string(bytes))
+
+	if json == nil && !json.IsFeatureCollection() {
+		t.FailNow()
+	}
+
+	opts := &TileOptions{Tolerance: 0, LineMetrics: true, Buffer: 2048, Extent: 4096, MaxZoom: 18, IndexMaxZoom: 5, IndexMaxPoints: 100000, GenerateId: false}
+
+	vt := NewGeoJSONVT(json, *opts)
+	tile := vt.GetTile(0, 0, 0)
+
+	fc := tile.GetFeatureCollection()
+
+	if tile == nil && !fc.Empty() {
+		t.FailNow()
+	}
+
+}

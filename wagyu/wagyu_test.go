936134cb0c37bdf6fa389972ca36427d8d4fa70d
aninggo <aninggo@gmail.com>
2021-08-09 10:36:28 +0800

    update

--- wagyu/wagyu_test.go
+++ wagyu/wagyu_test.go
@@ -17,6 +17,23 @@ func TestClipRing(t *testing.T) {
 
 	ctx := NewContext()
 
-	if ctx != nil && ring != nil {
+	solution := geojson.NewEmptyMultiPolygon()
+
+	ctx.AddRing(ring, PolygonTypeSubject)
+
+	f := ctx.Execute(ClipTypeUnion, solution, FillTypePositive, FillTypePositive)
+
+	if !f {
+		t.FailNow()
+	}
+
+	if solution.Count() != 1 {
+		t.FailNow()
+	}
+
+	raw := solution.Data()
+
+	if raw[0][0][2][1] != 1 {
+		t.FailNow()
 	}
 }
